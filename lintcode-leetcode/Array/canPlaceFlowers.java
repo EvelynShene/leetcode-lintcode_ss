a705cc380c5e5f64320e61a2883c0f3cba5dcdcc
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-06-08 15:37:24 -0400

    Update canPlaceFlowers.java

--- lintcode-leetcode/Array/canPlaceFlowers.java
+++ lintcode-leetcode/Array/canPlaceFlowers.java
@@ -1,6 +1,15 @@
+/** 1138. Can Place Flowers(lintcode *)/605. Can Place Flowers(leetcode)
+  * Suppose you have a long flowerbed in which some of the plots are planted and some are not. 
+  * However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
+  *
+  * Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), 
+  * and a number n, return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
+  * 
+  */
+
 public boolean canPlaceFlowers(int[] flowerbed, int n) {
     int availble = 0;
-    boolean f = false;
+    boolean first0 = false;
     boolean has1 = false;
     int zeron = 0;
 
@@ -8,14 +17,14 @@ public boolean canPlaceFlowers(int[] flowerbed, int n) {
         if(flowerbed[i] == 0){
             zeron ++;
             if(i == 0){
-                f= true;
+                first0 = true;
             }
         }
         if(flowerbed[i] == 1){
             has1 = true;
-            if(f){
+            if(first0){ // consider case like [0,0,1,...]
                 availble = availble + zeron/2;
-                f = false;
+                first0 = false;
             }
             else{
                 availble = availble + (zeron-1)/2;
@@ -24,10 +33,10 @@ public boolean canPlaceFlowers(int[] flowerbed, int n) {
         }
     }
     if(zeron != 0){
-        if(has1){
+        if(has1){ // consider case like [...1,0,0,0]
             availble = availble + zeron/2;
         }
-        else{
+        else{ // no 1 in original flowerbed
             availble = availble + (zeron+1)/2;
         }
     }
@@ -37,3 +46,8 @@ public boolean canPlaceFlowers(int[] flowerbed, int n) {
     else
         return false;
 }
+
+/**
+ * Note: (lintcode *) means it is easy to get accepted in lintcode, 
+ *        lack of test cases to test cases like [...1,0,0,0], [0,0,1,...] and [0,0,0].
+ */
