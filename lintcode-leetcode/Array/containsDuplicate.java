b0a179035657f320e6f091a8a0261fcab5e8d938
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-06-08 11:23:17 -0400

    Create containsDuplicate.java

---
+++ lintcode-leetcode/Array/containsDuplicate.java
@@ -0,0 +1,41 @@
+/** 1320. Contains Duplicate(lintcode)/217. Contains Duplicate(leetcode)
+  * Given an array of integers, find if the array contains any duplicates. 
+  * Your function should return true if any value appears at least twice in the array, 
+  * and it should return false if every element is distinct.
+  *
+  */
+  
+/** Method 1: AC in lintcode but "Time Limit Exceeded" in leetcode
+  * Time complexity is O(N^2)
+  */
+public boolean containsDuplicate(int[] nums) {
+    // Write your code here
+    for(int i = 0; i < nums.length; i++){
+        for(int j = 0; j < i; j++){
+            if(nums[i] == nums[j]){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+/** Method 2: AC in leetcode
+  * Time complexity is O(NlgN)
+  */
+public boolean containsDuplicate(int[] nums) {
+    Arrays.sort(nums);
+    for(int i = 1; i < nums.length; i++){
+        if(nums[i] == nums[i-1]){
+            return true;
+        }
+    }
+    return false;
+}
+/* Note: 若循环从0开始做 nums[i] = nums[i+1] 判断，则可能数组越界 */
+
+
+/** Method 3: Use Set (from Leetcode Discussion)
+  * Time complexity is O(N), but Memory = O(N)
+  */
+
