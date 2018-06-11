cea5eaf50703d5c8a3284eb2b3b09400f2fbfc50
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-06-11 18:37:18 -0400

    Update containsDuplicateII.java

--- lintcode-leetcode/Array/containsDuplicateII.java
+++ lintcode-leetcode/Array/containsDuplicateII.java
@@ -3,3 +3,15 @@
  *  For example, the input is [1, 0, 1, 1], nums[2] = 1, nums[3] = 1, thus i, j = 2, 3 and abs(i- j) <=1. 
  *  There exist two indexes match the requirements, thus the output is true.
  */
+
+public static boolean containsNearbyDuplicate(int[] nums, int k) {       
+  for(int i = 0; i < nums.length; i++){
+      for(int j = i+1; j < nums.length; j++){
+          if(nums[i] == nums[j] && (j-i)<=k ){
+            System.out.println("i:"+i+",j:"+j);
+              return true;
+          }
+      }
+  }
+  return false;
+}
