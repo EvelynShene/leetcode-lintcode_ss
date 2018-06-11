0fcf94ca3644ca458cd9caac8d1c77f1ca98fee7
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-06-11 18:30:03 -0400

    Create containsDuplicateII.java

---
+++ lintcode-leetcode/Array/containsDuplicateII.java
@@ -0,0 +1,5 @@
+/** this question is not about the maximum of absolute difference between i, j, 
+ *  but instead it is about whether there are two indexes i, j with nums[i] == nums[j] and abs(i - j) <= k. 
+ *  For example, the input is [1, 0, 1, 1], nums[2] = 1, nums[3] = 1, thus i, j = 2, 3 and abs(i- j) <=1. 
+ *  There exist two indexes match the requirements, thus the output is true.
+ */
