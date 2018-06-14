737bfce55a28101cc27f798740687a7932ccb244
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-06-14 12:03:44 -0400

    Update rotateArray.java

--- lintcode-leetcode/Array/rotateArray.java
+++ lintcode-leetcode/Array/rotateArray.java
@@ -10,9 +10,7 @@
 
 //Method 1: Using Extra Array
 public int[] rotate(int[] nums, int k) {
-    // Write your code here
-
-     
+    // Write your code here    
     int[] temp = new int[nums.length];
     int index = 0;
     k = k % nums.length;
@@ -30,4 +28,31 @@ public int[] rotate(int[] nums, int k) {
 
 }
 
-//Method 2: TODO
+//Method 2: Using Reverse
+/* idea: 
+ *    Original List                   : [1 2 3 4 5 6 7]
+ *    After reversing all numbers     : [7 6 5 4 3 2 1]
+ *    After reversing first k numbers : [5 6 7] 4 3 2 1
+ *    After revering last n-k numbers :  5 6 7 [1 2 3 4] --> Result
+ */
+
+public int[] rotate(int[] nums, int k) {
+    k = k % nums.length;
+
+    reverse(nums,0,nums.length-1);
+    reverse(nums,0,k-1);
+    reverse(nums,k,nums.length-1);
+
+    return nums;
+}
+
+public void reverse(int nums[],int l,int r){
+    if(l < 0 || r < 0){
+        return;
+    }
+    for(int i = l; i <= (r+l)/2; i++){
+        int temp = nums[i];
+        nums[i] = nums[r+l-i];
+        nums[r+l-i] = temp;
+    }
+}
