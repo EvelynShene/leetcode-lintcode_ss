889eeba493769ef2f3578daeeb826ff3a7a0a755
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-06-22 11:02:26 -0400

    Update combinationSum.java

--- lintcode-leetcode/Array/combinationSum.java
+++ lintcode-leetcode/Array/combinationSum.java
@@ -11,7 +11,7 @@
  *                        [2, 2, 3]
  */
  
- //Method: First sort and then use DFS
+ //Method: Backtracking - First sort and then use DFS
  public class Solution {
     /**
      * @param candidates: A list of integers
@@ -61,3 +61,25 @@
         }
     }
 }
+
+//Code from Leetcode Discuss, more concise
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+        if(remain < 0) return;
+        else if(remain == 0) list.add(new ArrayList<>(tempList));
+        else{ 
+            for(int i = start; i < nums.length; i++){
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+} 
