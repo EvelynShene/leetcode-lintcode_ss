1300ab40f450047c19065f2f8625ca0c9f63f6e7
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-06-28 22:39:32 -0400

    Create maximalRectangle.java

---
+++ lintcode-leetcode/Array/maximalRectangle.java
@@ -0,0 +1,13 @@
+/** 510. Maximal Rectangle(lintcode) / 85. Maximal Rectangle(leetcode)
+ *    Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+ *    
+ *    Example: Given a matrix:
+ *             [
+ *               [1, 1, 0, 0, 1],
+ *               [0, 1, 0, 0, 1],
+ *               [0, 0, 1, 1, 1],
+ *               [0, 0, 1, 1, 1],
+ *               [0, 0, 0, 0, 1]
+ *             ]
+ *             return 6.
+ */
