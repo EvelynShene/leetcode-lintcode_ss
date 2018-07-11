f3a1091cdfe4f15724e1be761d85eb6804033497
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-07-11 11:18:10 -0400

    Update longestPalindromicSubstring.java

--- lintcode-leetcode/String/longestPalindromicSubstring.java
+++ lintcode-leetcode/String/longestPalindromicSubstring.java
@@ -48,8 +48,43 @@
         return s.substring(index[0]);
     }
     return s.substring(index[0],index[1]);
- }
- 
+ } 
  /* 为简化代码，用函数findLongestPalindrome合并相同的代码: 
   *     findLongestPalindromeFrom(s, i, i) 和findLongestPalindromeFrom（s, i, i + 1）
   *／
+  
+/* Method 2: DP - O(n^2) time and space complexity
+ *    Idea: 一个字符串s(i,j)如果是回文字符串，那么s(i-1,j-1)一定是回文字符串。
+ *         1）用数组isP[i][j]指示字符串s(i,j)是否为字符串，初始化所有的单个字符isP[i][i] = true（所有字符本身可以构成回文字符串）
+ *         2）当遇到s(i) == s(j)时，isP[i][j]是否为回文字符串，取决于isP[i+1][j-1]是不是回文字符串。
+ *         3）由于isP[i][j] 需要 下一行的值isP[i+1][j-1]判断，所有行循环从第n行开始向上遍历。
+ */
+
+public String longestPalindrome(String s) {
+     if(s == null || s.length() == 0){
+         return "";
+     }
+     int n = s.length();
+     boolean[][] isP = new boolean[n][n];
+     int longest = 1;
+     int index = 0;
+     for(int i = 0; i < n; i++){
+         isP[i][i] = true;
+     }
+
+     for(int i = n - 1; i >= 0; i--){
+         for(int j = i + 1; j < n; j++){
+             if( s.charAt(i) == s.charAt(j) ){
+                 if( j - 1 == i || (i + 1 < n && isP[i + 1][j - 1])){ //j - 1 == i用来处理回文字符长度是偶数的情况
+                     isP[i][j] = true;
+                     if(longest < j - i + 1){
+                         longest = j - i + 1;
+                         index = i;
+                     }
+                 }
+             }
+         }
+     }
+
+     return s.substring(index,index + longest);
+ }
