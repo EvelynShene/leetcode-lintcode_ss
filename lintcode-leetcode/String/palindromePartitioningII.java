5457181b80bcb0d45420ebd48afa232edbb72981
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-08-06 16:51:21 -0400

    Create palindromePartitioningII.java

---
+++ lintcode-leetcode/String/palindromePartitioningII.java
@@ -0,0 +1,8 @@
+/** 132. Palindrome Partitioning II(leetcode) / 108. Palindrome Partitioning II(lintcode)
+ *      Given a string s, partition s such that every substring of the partition is a palindrome.
+ *  Return the minimum cuts needed for a palindrome partitioning of s.
+ *
+ *      Example: Input: "aab" ; Output: 1
+ *              Explanation: The palindrome partitioning ["aa","b"] could be produced using 1 cut.
+ */
+ //https://blog.csdn.net/jin_kwok/article/details/51423222
