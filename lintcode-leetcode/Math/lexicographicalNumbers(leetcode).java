c8d0d14a54c3b94e6c0b5d5ca544fbb74bd145c2
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-08-14 15:10:24 -0400

    Create lexicographicalNumbers(leetcode).java

---
+++ lintcode-leetcode/Math/lexicographicalNumbers(leetcode).java
@@ -0,0 +1,73 @@
+/** 386. Lexicographical Numbers(leetcode)
+ *    Given an integer n, return 1 - n in lexicographical order.
+ *
+ *    For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
+ *
+ *    Please optimize your algorithm to use less time and space. The input size may be as large as 5,000,000.
+ */
+ 
+ //Method 1: Sort - O(nlogn) time and O(n) space complexity
+ public List<Integer> lexicalOrder(int n) {
+    String[] s = new String[n];
+    for(int i = 1; i <= n; i++){
+        s[i - 1] = "" + i;
+    }
+    Arrays.sort(s);
+    List<Integer> list = new ArrayList<Integer>();
+    for(int i = 0; i < n; i++){
+        list.add(Integer.valueOf(s[i]));
+    }
+    return list;
+ }
+ 
+ //Method 2: Priority Queue ?? TLE (因为priority的offer()和poll()操作也是O(logn)的，所以以为可以AC，没想到超时了)
+ 
+ /* Method 3: 迭代 - [from leetcode]
+  *    从高位开始，一位一位的向下比较并排序
+  */
+ public List<Integer> lexicalOrder(int n) {
+    List<Integer> list = new ArrayList<>(n);
+    int curr = 1;
+    for (int i = 1; i <= n; i++) {
+        list.add(curr);
+        if (curr * 10 <= n) {
+            curr *= 10;
+        } else if (curr % 10 != 9 && curr + 1 <= n) {
+            curr++;
+        } else {
+            while ((curr / 10) % 10 == 9) {
+                curr /= 10;
+            }
+            curr = curr / 10 + 1;
+        }
+    }
+    return list;
+ }
+ 
+ /* Method 4: DFS - 本质思路同 Method 3 [from leetcode]
+  *            1        2        3    ...
+  *           /\        /\       /\
+  *        10 ...19  20...29  30...39   ....
+  */
+ public class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> res = new ArrayList<>();
+        for(int i=1;i<10;++i){
+          dfs(i, n, res); 
+        }
+        return res;
+    }
+    
+    public void dfs(int cur, int n, List<Integer> res){
+        if(cur>n)
+            return;
+        else{
+            res.add(cur);
+            for(int i=0;i<10;++i){
+                if(10*cur+i>n)
+                    return;
+                dfs(10*cur+i, n, res);
+            }
+        }
+    }
+}
