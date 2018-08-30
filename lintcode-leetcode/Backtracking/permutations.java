b3cba3ff4f5bb80534992800de7d4704b83dca9b
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-08-30 14:07:58 -0400

    Create permutations.java

---
+++ lintcode-leetcode/Backtracking/permutations.java
@@ -0,0 +1,37 @@
+/** 46. Permutations(leetcode)
+ *    Given a collection of distinct integers, return all possible permutations.
+ *
+ *    Example: Input: [1,2,3]
+ *             Output:
+ *                [
+ *                  [1,2,3],
+ *                  [1,3,2],
+ *                  [2,1,3],
+ *                  [2,3,1],
+ *                  [3,1,2],
+ *                  [3,2,1]
+ *                ]
+ */
+ 
+//Method: Backtracking = DFS with one variable assigned at each level [code from leetcode]
+public class solution(){
+    public List<List<Integer>> permute(int[] nums) {
+       List<List<Integer>> list = new ArrayList<>();
+       // Arrays.sort(nums); // not necessary
+       backtrack(list, new ArrayList<>(), nums);
+       return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+       if(tempList.size() == nums.length){
+          list.add(new ArrayList<>(tempList));
+       } else{
+          for(int i = 0; i < nums.length; i++){ 
+             if(tempList.contains(nums[i])) continue; // element already exists, skip, no duplicate number
+             tempList.add(nums[i]);
+             backtrack(list, tempList, nums);
+             tempList.remove(tempList.size() - 1);
+          }
+       }
+    } 
+}
