9fd3a43412571f7b5753e9d83782cbc4586e3070
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-09-28 16:19:17 -0400

    Update randomPickIndex.java

--- lintcode-leetcode/Random/randomPickIndex.java
+++ lintcode-leetcode/Random/randomPickIndex.java
@@ -41,3 +41,28 @@
  * Solution obj = new Solution(nums);
  * int param_1 = obj.pick(target);
  */
+
+//Method 2: Use reservoir sampling - less space[https://www.cnblogs.com/strugglion/p/6424874.html]
+class Solution {
+    int[] nums;
+    Random r;
+    public Solution(int[] nums) {
+        this.nums = nums;
+        r = new Random();
+    }
+    
+    public int pick(int target) {
+        int count = 0;
+        int res = -1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != target){
+                continue;
+            }
+            count++;
+            if(r.nextInt(count) == 0){
+                res = i;
+            }
+        }
+        return res;
+    }
+}
