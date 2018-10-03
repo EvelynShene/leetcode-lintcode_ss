b57a2514da278aa61e6b9f5fde733d6e95a5f7d6
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-10-03 11:04:05 -0400

    Update connectingGraph(lintcode).java

--- lintcode-leetcode/UnionFind/connectingGraph(lintcode).java
+++ lintcode-leetcode/UnionFind/connectingGraph(lintcode).java
@@ -49,7 +49,7 @@
     }
 }
 
-/* Method 2: union - O(lgn) ; query - O(lgn)
+/* Method 2: union - O(lgn)-O(n) ; query - O(lgn)
  *      引入了一个抽象的“树”结构，即初始时每个点都是一棵独立的树，所有的点构成了一个大森林。
  *    每一次连接，实际上就是两棵树的合并。通过，不断的合并，合并，再合并最后长成了一棵棵的大树。
  */
@@ -76,6 +76,58 @@ public class ConnectingGraph {
         int id_b = find(b);
         if(id_a != id_b){
             ids[id_a] = id_b;
+        }
+    }
+
+    public boolean query(int a, int b) {
+        // write your code here
+        return find(a) == find(b);
+    }
+}
+
+/** Method 3:
+ *   method2的问题：一种极端情况，即每连接一个点之后，树在不断往下生长，最后长成一棵“秃树”（没有任何树枝）
+ *   所以需要在两棵树进行连接之前做一个判断：每一次都优先选择将小树合并到大树下面，这样子树的高度不变，能避免树一直往下增长了
+ */
+public class ConnectingGraph {
+    int[] ids;
+    int[] height;
+    public ConnectingGraph(int n) {
+        // do intialization if necessary
+        ids = new int[n + 1];
+        height = new int[n + 1];
+        for(int i = 1; i <= n; i++){
+            ids[i] = i;
+            height[i] = 1;
+        }
+    }
+    
+    public int find(int a){
+        while(ids[a] != a){
+            a = ids[a];
+        }
+        return a;
+    }
+    
+    public void connect(int a, int b) {
+        // write your code here
+        int root_a = find(a);
+        int root_b = find(b);
+        if(root_a == root_b){
+            return;
+        }
+        if(height[root_a] > height[root_b]){
+            ids[root_b] = root_a;
+            height[root_b] = height[root_b];
+        }
+        else if(height[root_a] < height[root_b]){
+            ids[root_a] = root_b;
+            height[root_a] = height[root_b];
+        }
+        else{
+            ids[root_a] = root_b;
+            height[root_a]++;
+            height[root_b]++;
         }
     }
 
