9069cdffaf00f192cd1c1615a75efb640b1de4f7
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-10-28 16:54:35 -0400

    Update nextClosestTime.java

--- lintcode-leetcode/String/nextClosestTime.java
+++ lintcode-leetcode/String/nextClosestTime.java
@@ -15,38 +15,57 @@
  /* My Method(Idea) - 找规律 
   *     1） 对于时间的四个数字num[i],在四个数中找出每个数字的比它大得最小数字，存入leastmax[i]数组中。(leastmax[i] = 10是最大值)
   *     2） 时间的每一位都有一个最大取值{2,4,5,9}，存在max[]数组中。
+  *       Note:如果最高位是1，那么第二位可以取1-9任意数！eg: Input: 13:55 ; Output: 15:11
   *     3） 从后往前遍历，找到第一个leastmax[i]在当前时间位置的可取范围内(最大值不计),把当前位置值换成它的最小大于值。
   *     4） 置换位之后的位置的值，全部有最小值min替代。
   */
  
- public static String nextClosestTime(String time) {
-    int[] num = new int[4];
-    int[] max = {2,4,5,9};
+public String nextClosestTime(String time) {
+    int[] nums = new int[4];
+    int[] max = {2, 3, 5, 9};
     int min = 10;
-    num[0] = time.charAt(0) - '0';
-    num[1] = time.charAt(1) - '0';
-    num[2] = time.charAt(3) - '0';
-    num[3] = time.charAt(4) - '0';
+    nums[0] = time.charAt(0) - '0';
+    nums[1] = time.charAt(1) - '0';
+    nums[2] = time.charAt(3) - '0';
+    nums[3] = time.charAt(4) - '0';
 
-    int[] leastmax = {10, 10, 10, 10};
+    int[] leastMax = {10, 10, 10, 10};
     for(int i = 0; i < 4; i++){
+        min = Math.min(min, nums[i]);
         for(int j = 0; j < 4; j++){
-          if(i != j && num[j] > num[i] && num[j] < leastmax[i]){
-            leastmax[i] = num[j];
-          }
+            if(nums[j] > nums[i]){
+                leastMax[i] = Math.min(nums[j], leastMax[i]);
+            }
         }
-        min = Math.min(min,num[i]);
     }
 
-    for(int i = 3; i >= 0; i--){
-        if(leastmax[i] != 10 && leastmax[i] <= max[i]){
-            num[i] = leastmax[i];
+    int index = 3;
+    while(index >= 0){
+     if(index == 1){
+      if(nums[0] != 2){
+       if(leastMax[index] != 10){
+        nums[index] = leastMax[index];
+        index++;
+        break;
+       }
+      }
+     }
+     if(leastMax[index] != 10 && leastMax[index] <= max[index]){
+            nums[index] = leastMax[index];
+            index++;
             break;
         }
-        else{
-            num[i] = min;
-        }
+        index--;
     }
-    String s = "" + num[0] + num[1] + ":" + num[2] + num[3];
-    return s;
- }
+    // System.out.println(index);
+    if(index < 0){
+        index++;
+    }
+
+    while(index < 4){
+        nums[index] = min;
+        index++;
+    }
+    String res = "" + nums[0] + nums[1] + ":" + nums[2] + nums[3];
+    return res;
+}
